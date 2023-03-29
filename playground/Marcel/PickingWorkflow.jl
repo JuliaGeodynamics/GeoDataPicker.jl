6c453ebfb88bac20e191deb3a030dd7d9466c5cf
mthielma <thielmann.marcel@gmail.com>
2023-03-29 10:15:31 +0200

    update
    - fixed a bug

--- playground/Marcel/PickingWorkflow.jl
+++ playground/Marcel/PickingWorkflow.jl
@@ -1,5 +1,7 @@
 # workflow for data picking
 # WORKAROUND FOR NOW: USE THE PROVIDED MATLAB APP
+using GeophysicalModelGenerator
+
 
 # define the data sets that are to be used
 ProfileCoordFile = "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing/TestSave.txt"
@@ -7,6 +9,7 @@ ProfileNumber    = 3;
 
 DataSetName = [ "TomoPaffrath2021"
                 "TomoPiromallo2003"
+                "TomoMitterbauer"
                 "MohoAD_Mrokzek2022"
                 "MohoEU_Mrokzek2022"
                 "MohoPA_Mrokzek2022"
@@ -15,6 +18,7 @@ DataSetName = [ "TomoPaffrath2021"
 
 DataSetFile = [ "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing/Paffrath2021/Paffrath2021.jld2"
                 "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing/Piromallo2003/Piromallo2003.jld2"
+                "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing/Mitterbauer2011/Mitterbauer2011.jld2"
                 "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing//Moho_models/Mrozek2022/Mrozek_Moho_Grid_EU.jld2"
                 "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing//Moho_models/Mrozek2022/Mrozek_Moho_Grid_AD.jld2"
                 "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing//Moho_models/Mrozek2022/Mrozek_Moho_Grid_PA.jld2"
@@ -24,13 +28,14 @@ DataSetFile = [ "/Users/mthiel/PROJECTS/CURRENT/SPP2017/TomographyProcessing/Paf
 
 DataSetType = [ "Vol"
                 "Vol"
+                "Vol"
                 "Surf"
                 "Surf"
                 "Surf"
                 "Point"
                 ]
 
-DimsVolCross        = (100,100)
+DimsVolCross        = (200,200)
 DimsSurfCross       = (100,)
 WidthPointProfile   = 10km
 
